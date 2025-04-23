d1b01a85488935079221f86b6c0fb7bfdf771b9e
Reshav Sharma <2022bit061@sggs.ac.in>
2025-04-24 00:59:58 +0530

    Added Reshav_Sharma's Projct_1

---
+++ Reshav_Sharma_Project_1/CL_3_Project_App_Expense_Tracker/expense_tracker/tracker/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('index/', views.index, name='index'),
+    path('add/', views.add_expense, name='add_expense'),
+    path('edit/<int:id>/', views.edit_expense, name='edit_expense'),
+    path('delete/<int:id>/', views.delete_expense, name='delete_expense'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('logout/', views.logout_view, name='logout'),
+
+]
