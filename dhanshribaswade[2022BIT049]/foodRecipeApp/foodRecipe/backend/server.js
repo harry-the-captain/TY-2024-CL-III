e1a8a44803371069c70f9c8f33b113adb8c0c261
gitdhanshri <2022bit049@sggs.ac.in>
2025-04-24 20:47:10 +0530

    Add file

---
+++ dhanshribaswade[2022BIT049]/foodRecipeApp/foodRecipe/backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config(); // load .env variables
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("MongoDB connected");
+}).catch((err) => {
+  console.error("MongoDB connection error:", err);
+});
